26569664057618eb35507ec73cb5ae20acfaf0a6
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-10-31 14:13:16 +0800

    fixing details into variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,16 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IronMan from "../images/ironman.jpg"
 
+const SaleDate = "15/05/1998"
+const CutoffDate = "15/05/1998"
+const UnlimitedDate = "15/05/1998"
+const DigitalDate = "15/05/1998"
+const Writer = "Youssef"
+const Penciller = "El-Moukhtar"
+const Letterer = "Mokhtari"
+const Summary = "A long time ago in a galaxy far far away"
+
 const IndexPage = () => (
-  // <Layout>
-  //   <SEO title="Home" />
-  //   <h1>Hi Marvel</h1>
-  //   <p>Let's have some fun</p>
-  //   <p>Now go build something great.</p>
-  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-  // <Image />
-  //   </div>
-  //   <Link to="/page-2/">Go to page 2</Link>
-  // </Layout>
   <Layout>
     <div class="row">
       <div class="column" style={{ backgroundColor: "#e0e0e0" }}>
@@ -27,16 +26,28 @@ const IndexPage = () => (
         <div class="babycolumn">
           <h3>test1</h3>
           <p>Some text..</p>
+          <img src={IronMan} style={{ width: "150px" }}></img>
         </div>
         <div class="babycolumn" style={{ backgroundColor: "#e0e0e0" }}>
           <h3>test2</h3>
-          <div>
-            <img
-              src={IronMan}
-              style={{ width: "150px", borderRadius: "75px" }}
-            ></img>
-          </div>
-          <p>Some text..</p>
+
+          <p>
+            <b>Date on sale:</b> {SaleDate}
+            <br></br>
+            <b>Final cut off order date:</b> {CutoffDate}
+            <br></br>
+            <b>Unlimited date:</b> {UnlimitedDate}
+            <br></br>
+            <b>Digital purchase date:</b> {DigitalDate}
+            <br></br>
+            <b>Writer:</b> {Writer}
+            <br></br>
+            <b>Penciller:</b> {Penciller}
+            <br></br>
+            <b>Letterer:</b> {Letterer}
+            <br></br> <b>Summary:</b>
+          </p>
+          <p> {Summary}</p>
         </div>
       </div>
     </div>
